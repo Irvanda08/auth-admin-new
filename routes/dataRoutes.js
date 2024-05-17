f0057f1e5760cfb97ff92a73cb2fc7339c441cc2
Benedictus Irvanda Nugroho <irvandanugroho08@gmail.com>
2024-05-17 19:03:50 +0700

    Update dataRoutes.js

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const dataController = require("../controllers/controllerData");
+
 const adminController = require("../controllers/controllerAdmin");
 const authMiddleware = require("../middleware/authMiddleware");
 
