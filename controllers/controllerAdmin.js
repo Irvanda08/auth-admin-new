b5804bf855c53ce8130b8e2e471f722460c8d85f
Benedictus Irvanda Nugroho <irvandanugroho08@gmail.com>
2024-06-05 15:08:06 +0700

    Update controllerAdmin.js

--- controllers/controllerAdmin.js
+++ controllers/controllerAdmin.js
@@ -32,46 +32,35 @@ exports.findById = (req, res) => {
   Admin.findById(req.params.username, (err, data) => {
     if (err) {
       res.status(500).send({ message: "Error fetching data" });
-    } else if (!data || data.length === 0) {
-      res.status(404).send({ message: "Admin not found" });
     } else {
-      res.json(data[0]);
+      res.json(data);
     }
   });
 };
 
 exports.create = async (req, res) => {
-  const { username, password } = req.body;
-
-  if (!username || !password) {
-    return res.status(400).send({ message: "Please provide all required fields" });
-  }
-
+  const newAdmin = new Admin(req.body);
   try {
-    // Check if the username already exists
-    Admin.findById(username, async (err, data) => {
-      if (err) {
-        return res.status(500).send({ message: "Error fetching data" });
-      }
-
-      if (data.length > 0) {
-        return res.status(400).send({ message: "Username already exists" });
-      }
-
-      const newAdmin = new Admin({ username, password });
-
-      // Create the new admin
-      Admin.create(newAdmin, (err, data) => {
-        if (err) {
-          return res.status(500).send({ message: "Error creating data" });
-        } else {
-          return res.json({ message: "Admin added successfully", data });
-        }
-      });
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newAdmin = new Admin({
+      username: req.body.username,
+      password: hashedPassword,
     });
-  } catch (error) {
-    return res.status(500).send({ message: "Error creating data" });
+
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(400).send({ message: "Please provide all required fields" });
+  } else {
+    Admin.create(newAdmin, (err, data) => {
+      if (err) {
+        res.status(500).send({ message: "Error creating data" });
+      } else {
+        res.json({ message: "Admin added successfully", data });
+      }
+    });
   }
+} catch (error) {
+  res.status(500).send({ message: "Error creating data" });
+}
 };
 
 exports.findAll = (req, res) => {
@@ -88,8 +77,6 @@ exports.delete = (req, res) => {
   Admin.delete(req.params.username, (err, data) => {
     if (err) {
       res.status(500).send({ message: "Error deleting data" });
-    } else if (data.affectedRows === 0) {
-      res.status(404).send({ message: "Admin not found" });
     } else {
       res.json({ message: "Admin deleted successfully" });
     }
