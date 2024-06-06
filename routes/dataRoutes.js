ddc01abebe8d14aa35186490da7e476b35b409e6
Benedictus Irvanda Nugroho <irvandanugroho08@gmail.com>
2024-06-06 09:13:05 +0700

    Update dataRoutes.js

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -9,6 +9,4 @@ router.post("/admin/login", adminController.login);
 router.get("/admin", adminController.findAll);
 router.delete("/admin/:username", adminController.delete);
 
-router.get("/admin/check-username", adminController.checkUsername);
-
 module.exports = router;
